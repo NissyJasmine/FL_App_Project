f43b14fb5350b652a39c09bc65d895949fefd6b7
NissyJasmine <nissyjasmineramagiri@gmail.com>
2026-04-07 14:18:26 +0530

    Create index.py

---
+++ index.py
@@ -0,0 +1,16 @@
+import os
+import sys
+from streamlit.web.cli import main
+
+if __name__ == "__main__":
+    # This forces Streamlit to run on the port Vercel provides
+    sys.argv = [
+        "streamlit",
+        "run",
+        "dashboard.py",
+        "--server.port",
+        "8080",
+        "--server.address",
+        "0.0.0.0",
+    ]
+    sys.exit(main())
